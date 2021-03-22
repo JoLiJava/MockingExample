846e756cd875e1c4454c1bc271af6aa84ea151fe
JoLiJava <73590210+JoLiJava@users.noreply.github.com>
2021-03-22 11:14:46 +0100

    Uppgift 1 Step1 - Created simple String calculator with three tests

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,22 @@
+package calculator;
+
+public class Calculator {
+
+    // String test = "2,4";
+
+    public int add(String numbers) {
+
+        String[] arrayNumbers;
+        int addingNumbers = 0;
+
+        if (numbers.equals("")) {
+            return 0;
+        }
+
+        arrayNumbers = numbers.split(",");
+        for (String arrayNumber : arrayNumbers) {
+            addingNumbers += Integer.parseInt(arrayNumber);
+        }
+        return addingNumbers;
+    }
+}
---
+++ src/test/java/calculator/CalculatorTest.java
@@ -0,0 +1,33 @@
+package calculator;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+
+    Calculator calculator = new Calculator();
+
+    @Test
+    @DisplayName("Adding two numbers")
+    void addingTwoNumbers() {
+
+        assertEquals(3, calculator.add("1,2"));
+
+    }
+
+    @Test
+    @DisplayName("Adding one number")
+    void addingOneNumber() {
+
+        assertEquals(1, calculator.add("1"));
+    }
+
+    @Test
+    @DisplayName("Adding empty string")
+    void addingEmptyString() {
+
+        assertEquals(0, calculator.add(""));
+    }
+}
