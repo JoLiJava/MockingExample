83beff72918cfb41f2d9bdc381d7834b46b25881
JoLiJava <73590210+JoLiJava@users.noreply.github.com>
2021-03-22 15:18:40 +0100

    Uppgift 1 Step 5 - Add method now throws exception on negative numbers

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -8,13 +8,26 @@ public class Calculator {
 
         String[] arrayNumbers;
         int addingNumbers = 0;
+        boolean illegalException = false;
+        StringBuilder negativeNumbers = new StringBuilder();
 
         arrayNumbers = numbers.split("[/\n;,]");
-        for (String arrayNumber : arrayNumbers) {
-            if (!arrayNumber.isEmpty()) {
-                addingNumbers += Integer.parseInt(arrayNumber);
+        for (String number : arrayNumbers) {
+            if (!number.isEmpty()) {
+                addingNumbers += Integer.parseInt(number);
+
+                if (Integer.parseInt(number) < 0) {
+                    illegalException = true;
+                    negativeNumbers.append(" ").append(number);
+
+                }
             }
         }
+
+        if (illegalException) {
+            throw new IllegalArgumentException("negatives not allowed" + negativeNumbers);
+        }
+
         return addingNumbers;
     }
 }
